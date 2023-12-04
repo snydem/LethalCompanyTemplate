2ec8e696333c0a4a68913c7c83f8c72376d8b1ea
snydem <cs059588@gmail.com>
2023-12-03 21:25:21 -0500

    First time commit

---
+++ LethalCompanyTemplate/obj/Debug/netstandard2.1/PluginInfo.cs
@@ -0,0 +1,9 @@
+namespace LethalCompanyTemplate
+{
+    public static class PluginInfo
+    {
+        public const string PLUGIN_GUID = "DoppleGangerMod";
+        public const string PLUGIN_NAME = "DoppleGangerMod";
+        public const string PLUGIN_VERSION = "1.0.0";
+    }
+}
